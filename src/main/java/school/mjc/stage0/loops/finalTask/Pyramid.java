53c24f1087d1889c875dcf4f30176d0b78f6dd12
odilhon85 <i.o.r@inbox.ru>
2022-07-25 13:00:49 +0500

    Changes done 1-attempt

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,10 +2,28 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        for (int i = 1; i <=cathetusLength ; i++) {
+            for (int j = 1; j <=cathetusLength*2-1 ; j++) {
+                if(j<=cathetusLength-i || j>=cathetusLength+i){
+                    System.out.print(" ");
+                }else{
+                    if(j==cathetusLength){
+                        System.out.print(1);
+                    }
+                    if(j>cathetusLength){
+                        System.out.print(1+(j-cathetusLength));
+                    }
+                    if(j<cathetusLength){
+                        System.out.print(cathetusLength-j+1);
+                    }
+                }
 
+            }
+            System.out.println("");
+        }
     }
 
     public static void main(String[] args) {
-        new Pyramid().printPyramid(7);
+        new Pyramid().printPyramid(0);
     }
 }
